98860b9731092f4034e17bf5da531063f7f0b4c5
tlsrhksgh <sin99145@naver.com>
2023-07-07 14:18:16 +0900

    프록시 팩토리 적용

---
+++ src/test/java/hello/proxy/common/advice/TimeAdvice.java
@@ -0,0 +1,25 @@
+package hello.proxy.common.advice;
+
+import lombok.extern.slf4j.Slf4j;
+import org.aopalliance.intercept.MethodInterceptor;
+import org.aopalliance.intercept.MethodInvocation;
+
+@Slf4j
+public class TimeAdvice implements MethodInterceptor {
+
+    @Override
+    public Object invoke(MethodInvocation invocation) throws Throwable {
+        log.info("TimeProxy 실행");
+        long startTime = System.currentTimeMillis();
+
+//      Object result = method.invoke(target, args);
+//      invocation.proceed()를 호출하면 알아서 target을 찾아주고 인수를 넘기면서 실행을 해준다. 위와 동일
+        Object result = invocation.proceed();
+
+
+        long endTime = System.currentTimeMillis();
+        long resultTime = endTime - startTime;
+        log.info("TimeProxy 종료 resultTime={}", resultTime);
+        return result;
+    }
+}
